3a1431feb2ad9fd36c8835f4186aa7bb71a876ad
mycatpablo <mmalpique06@gmail.com>
2025-05-02 21:33:48 +0100

    add conversor binário/decimal, add binds para botões

--- converter.py
+++ converter.py
@@ -1,42 +1,91 @@
 # O QUE FALTA:
-# - Conversão de binário para decimal
-# - Botão para poder alternar dos dois modos (decimal/binário, binário/decimal)
-# - Alterações na interface só para não ficar tão feio :(
+# - Comentar código
 
 from tkinter import * # Importa a biblioteca Tkinter, que é preciso para criar a interface gráfica
 
-def calculation():
-   
-    val1 = int(entry1.get())
-    val2 = int(entry2.get())
-    val3 = int(entry3.get())
-    val4 = int(entry4.get())
+class Converter:
+    def __init__(self):
+        self.is_decimal = True
+    
+    def conversion(self):
+        self.is_decimal = not self.is_decimal
 
-    if any(entry > 255 or entry < 0 for entry in [val1, val2, val3, val4]):
-        ip_binary = "Erro: Valor acima de 255"
-    else:
-        result1 = bin(int(entry1.get()))[2:].zfill(8)
-        result2 = bin(int(entry2.get()))[2:].zfill(8)
-        result3 = bin(int(entry3.get()))[2:].zfill(8)
-        result4 = bin(int(entry4.get()))[2:].zfill(8)
-        
-        ip_binary = f"{result1}.{result2}.{result3}.{result4}"
+converter = Converter()
+
+def calculation():
+
+    try:
+        val1 = int(entry1.get())
+        val2 = int(entry2.get())
+        val3 = int(entry3.get())
+        val4 = int(entry4.get())
+
+        if any((entry > 255 or entry < 0) if converter.is_decimal else (entry > 11111111 or entry < 0) for entry in [val1, val2, val3, val4]):
+            ip_binary = "Erro: Valor acima de 255" if converter.is_decimal else "Erro: Valor acima de 11111111"
+        else:
+            if converter.is_decimal:
+                result1 = bin(int(entry1.get()))[2:].zfill(8)
+                result2 = bin(int(entry2.get()))[2:].zfill(8)
+                result3 = bin(int(entry3.get()))[2:].zfill(8)
+                result4 = bin(int(entry4.get()))[2:].zfill(8)
+                ip_binary = f"{result1}.{result2}.{result3}.{result4}"
+            else:
+                ip_binary = f"{int(entry1.get(), 2)}.{int(entry2.get(), 2)}.{int(entry3.get(), 2)}.{int(entry4.get(), 2)}"
+
+    except ValueError:
+        ip_binary = "Erro: Não foi introduzido nenhum valor!"
 
     result_label.config(text=ip_binary)
 
 def only_numbers(char):
     return char.isdigit()
 
-def max_numbers(num):
+def jump_cursor(event):
+    next_entry = event.tk_focusNext()
+    next_entry.focus_set()
+
+def max_numbers(value):
     for entry in [entry1, entry2, entry3, entry4]:
         value = entry.get()
-        if value and int(value) > 255:
-            entry.delete(3, END)
+        if converter.is_decimal:
+            if value and len(value) > 2:
+                jump_cursor(entry)
+        else:
+            if value and len(value) > 7:
+                jump_cursor(entry)
+
+def clear_button_click():
+    entry1.delete(0, END)
+    entry2.delete(0, END)
+    entry3.delete(0, END)
+    entry4.delete(0, END)
+    result_label.config(text='')
+    entry1.focus_set()
+
+def copy_result():
+    result_text = result_label.cget("text")
+    window.clipboard_clear()
+    window.clipboard_append(result_text)
+    copybtn.config(text='Copiado!')
+    window.after(1000, lambda: copybtn.config(text='Copiar para o teclado'))
+
+def toggle_button_click():
+    entry1.delete(0, END)
+    entry2.delete(0, END)
+    entry3.delete(0, END)
+    entry4.delete(0, END)
+    result_label.config(text='')
+    entry1.focus_set()
+    converter.conversion()
+    if converter.is_decimal:
+        togglebtn.config(text='Trocar para binário')
+    else:
+        togglebtn.config(text='Trocar para decimal')
 
 window = Tk() # cria a raiz Tk
 
-w = 350 # largura para a raiz Tk
-h = 470 # altura para a raiz Tk
+w = 675 # largura para a raiz Tk
+h = 300 # altura para a raiz Tk
 
 # aqui, recebemos a altura e a largura do nosso ecrã
 ws = window.winfo_screenwidth() # largura do ecrã
@@ -51,8 +100,14 @@ window.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
 window.title("Conversor de IP") # título da janela
 
-vcmd = (window.register(only_numbers), '%S')
+window.resizable(False, False)
 
+window.bind("<Escape>", lambda event: clear_button_click())
+window.bind("<Return>", lambda event: calculation())
+window.bind("<c>", lambda event: copy_result())
+window.bind("<t>", lambda event: toggle_button_click())
+
+vcmd = (window.register(only_numbers), '%S')
 
 initial_text = Label(window, font=('Arial', 20), bg='lightgray', width=10, justify='center')
 initial_text.grid(row=0, column=1)
@@ -62,6 +117,7 @@ initial_text.grid(row=0, column=1)
 entry1 = Entry(window, font=('Arial', 20), bg='lightgray', width=10, justify='center', validate='key', validatecommand=vcmd)
 entry1.grid(row=0, column=1)
 entry1.bind("<KeyRelease>", max_numbers)
+entry1.focus_set()
 
 dot1 = Label(window, font=('Arial', 20), text='.')
 dot1.grid(row=0, column=2)
@@ -85,9 +141,18 @@ entry4.grid(row=0, column=7)
 entry4.bind("<KeyRelease>", max_numbers)
 
 convertbtn = Button(window, text='Converter', font=('Arial', 20), width=10, command=calculation)
-convertbtn.grid(row=1, column=3, columnspan=3, pady=(10, 0))
+convertbtn.grid(row=1, column=2, columnspan=2)
+
+clearbtn = Button(window, text='Limpar', font=('Arial', 20), width=10, command=lambda: clear_button_click())
+clearbtn.grid(row=1, column=3, columnspan=11)
+
+copybtn = Button(window, text='Copiar para o teclado', font=('Arial', 20), width=17, command = copy_result)
+copybtn.grid(row=2, column=2, columnspan=4)
+
+togglebtn = Button(window, text='Trocar para binário', font=('Arial', 20), width=15, command= lambda: toggle_button_click())
+togglebtn.grid(row=3, column=2, columnspan=4)
 
 result_label = Label(window, text='', font=('Arial', 20))
-result_label.grid(row=2, column=3, columnspan=8, pady=(10,0))
+result_label.grid(row=4, column=1, columnspan=8)
 
 window.mainloop()
